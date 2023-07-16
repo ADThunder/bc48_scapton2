7da69b9200f8101c4895a68ed80dd7d2a438db63
ADThunder <maianhdung02051996@gmail.com>
2023-07-17 03:38:38 +0700

    clear form

--- JS/form.js
+++ JS/form.js
@@ -14,6 +14,7 @@ postUser = (duLieu) => {
     const toastLiveExample = document.getElementById("liveToast");
     const toast = new bootstrap.Toast(toastLiveExample);
     toast.show();
+    document.getElementById('textForm').reset();
   });
   promise.catch((err) => {
     // dom gửi thông báo lên cho người dùng
